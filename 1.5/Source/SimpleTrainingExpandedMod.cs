5e087b82c9d0c319258556c5af2893e6087ac3fb
Taranchuk <nfhfyxer@gmail.com>
2024-09-23 21:25:37 +0300

    update

---
+++ 1.5/Source/SimpleTrainingExpandedMod.cs
@@ -0,0 +1,66 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Verse;
+using Verse.AI;
+
+namespace SimpleTrainingExpanded
+{
+    public class SimpleTrainingExpandedMod : Mod
+    {
+        public SimpleTrainingExpandedMod(ModContentPack pack) : base(pack)
+        {
+			new Harmony("SimpleTrainingExpandedMod").PatchAll();
+        }
+    }
+
+    public class WorkGiver_Train : WorkGiver_Scanner
+    {
+
+    }
+
+    public class JobDriver_Train : JobDriver
+    {
+        public override bool TryMakePreToilReservations(bool errorOnFailed)
+        {
+            return pawn.Reserve(TargetA, job);
+        }
+
+        public override IEnumerable<Toil> MakeNewToils()
+        {
+
+        }
+    }
+
+    public class CompProperties_TrainSkills : CompProperties
+    {
+        public List<SkillTrain> skillsToTrainPerTick;
+        public float joyPerTick;
+        public float learningPerTick;
+        public JobDef trainingJob;
+        public bool rangedWeaponRequired;
+        public bool meleeWeaponRequired;
+
+        public CompProperties_TrainSkills()
+        {
+            this.compClass = typeof(CompTrainSkills);
+        }
+    }
+
+    public class CompTrainSkills : ThingComp
+    {
+        public CompProperties_TrainSkills Props => base.props as CompProperties_TrainSkills;
+
+        public void PawnTrainTick(Pawn pawn)
+        {
+            foreach (var skillToTrain in Props.skillsToTrainPerTick)
+            {
+                var skill = pawn.skills.GetSkill(skillToTrain.skill);
+                skill.Learn(skillToTrain.xp);
+            }
+            JoyUtility.JoyTickCheckEnd(pawn);
+        }
+    }
+}
---
+++ 1.5/Source/SkillTrain.cs
@@ -0,0 +1,34 @@
+﻿using RimWorld;
+using System.Xml;
+using Verse;
+
+namespace SimpleTrainingExpanded
+{
+    public class SkillTrain
+    {
+        public SkillDef skill;
+
+        public float xp;
+
+        public SkillTrain()
+        {
+        }
+
+        public SkillTrain(SkillDef skill, float xp)
+        {
+            this.skill = skill;
+            this.xp = xp;
+        }
+
+        public void LoadDataFromXmlCustom(XmlNode xmlRoot)
+        {
+            if (xmlRoot.ChildNodes.Count != 1)
+            {
+                Log.Error("Misconfigured SkillGain: " + xmlRoot.OuterXml);
+                return;
+            }
+            DirectXmlCrossRefLoader.RegisterObjectWantsCrossRef(this, "skill", xmlRoot.Name);
+            xp = ParseHelper.FromString<float>(xmlRoot.FirstChild.Value);
+        }
+    }
+}
