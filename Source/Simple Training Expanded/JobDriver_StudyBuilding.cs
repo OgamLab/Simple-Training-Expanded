68447512ee9d8a410f5aa67999430aa25314e35c
MrHydralisk <113328125+MrHydralisk@users.noreply.github.com>
2025-03-13 05:59:14 +0300

    Initial Skill training mode

--- Source/Simple Training Expanded/JobDriver_StudyBuilding.cs
+++ Source/Simple Training Expanded/JobDriver_StudyBuilding.cs
@@ -1,14 +1,21 @@
 ﻿using RimWorld;
 using System.Collections.Generic;
+using UnityEngine;
 using Verse;
 using Verse.AI;
-using Verse.Sound;
 
 namespace SimpleTrainingExpanded
 {
     public class JobDriver_StudyBuilding : JobDriver
     {
-        private const int ShotDuration = 600;
+        private const TargetIndex BuildingTargetInd = TargetIndex.A;
+        private const TargetIndex CellTargetInd = TargetIndex.B;
+
+        public Building building => TargetThingA as Building;
+        public SkillDef skillDef => job.def.joySkill;
+
+        private Effecter effecter = null;
+        public bool isNotForJoy = false;
 
         public override bool TryMakePreToilReservations(bool errorOnFailed)
         {
@@ -17,43 +24,92 @@ namespace SimpleTrainingExpanded
 
         protected override IEnumerable<Toil> MakeNewToils()
         {
-            this.EndOnDespawnedOrNull(TargetIndex.A);
-            Toil chooseCell = Toils_Misc.FindRandomAdjacentReachableCell(TargetIndex.A, TargetIndex.B);
+            this.EndOnDespawnedOrNull(BuildingTargetInd);
+            Toil chooseCell = Toils_Misc.FindRandomAdjacentReachableCell(BuildingTargetInd, CellTargetInd);
             yield return chooseCell;
-            yield return Toils_Reserve.Reserve(TargetIndex.B);
-            yield return Toils_Goto.GotoCell(TargetIndex.B, PathEndMode.OnCell);
-            Toil toil = ToilMaker.MakeToil("MakeNewToils");
-            toil.initAction = delegate
+            yield return Toils_Reserve.Reserve(CellTargetInd);
+            yield return Toils_Goto.GotoCell(CellTargetInd, PathEndMode.OnCell);
+            Toil StudyToil = ToilMaker.MakeToil("StudyToils");
+            StudyToil.initAction = delegate
             {
                 job.locomotionUrgency = LocomotionUrgency.Walk;
             };
-            toil.tickAction = delegate
+            StudyToil.tickAction = delegate
             {
-                pawn.rotationTracker.FaceCell(base.TargetA.Thing.OccupiedRect().ClosestCellTo(pawn.Position));
+                pawn.rotationTracker.FaceCell(building.OccupiedRect().ClosestCellTo(pawn.Position));
                 //if (ticksLeftThisToil == 300)
                 //{
                 //    SoundDefOf.PlayBilliards.PlayOneShot(new TargetInfo(pawn.Position, pawn.Map));
                 //}
-                if (Find.TickManager.TicksGame > startTick + job.def.joyDuration)
+                if (isNotForJoy)
                 {
-                    EndJobWith(JobCondition.Succeeded);
+                    pawn.skills.Learn(skillDef, 0.1f);
+                    pawn.GainComfortFromCellIfPossible(chairsOnly: true);
+                    if (pawn?.skills?.GetSkill(skillDef)?.LearningSaturatedToday ?? true)
+                    {
+                        EndJobWith(JobCondition.Succeeded);
+                    }
+                    if (effecter == null)
+                    {
+                        EffecterDef progressBar = EffecterDefOf.ProgressBar;
+                        effecter = progressBar.Spawn();
+                    }
+                    else
+                    {
+                        effecter.EffectTick(pawn, TargetInfo.Invalid);
+                        MoteProgressBar mote = ((SubEffecter_ProgressBar)effecter.children[0]).mote;
+                        if (mote != null)
+                        {
+                            mote.progress = Mathf.Clamp01(pawn?.skills?.GetSkill(skillDef)?.XpProgressPercent ?? 0f);
+                            mote.offsetZ = -0.5f;
+                        }
+                    }
                 }
                 else
                 {
-                    JoyUtility.JoyTickCheckEnd(pawn, JoyTickFullJoyAction.EndJob, 1f, (Building)base.TargetThingA);
+                    if (Find.TickManager.TicksGame > startTick + job.def.joyDuration)
+                    {
+                        EndJobWith(JobCondition.Succeeded);
+                    }
+                    else
+                    {
+                        JoyUtility.JoyTickCheckEnd(pawn, JoyTickFullJoyAction.EndJob, 1f, building);
+                    }
                 }
             };
-            toil.handlingFacing = true;
-            toil.socialMode = RandomSocialMode.SuperActive;
-            toil.defaultCompleteMode = ToilCompleteMode.Delay;
-            toil.defaultDuration = 600;
-            toil.AddFinishAction(delegate
+            StudyToil.handlingFacing = true;
+            StudyToil.socialMode = RandomSocialMode.SuperActive;
+            StudyToil.defaultCompleteMode = ToilCompleteMode.Delay;
+            StudyToil.defaultDuration = isNotForJoy ? 4000 : 600;
+            StudyToil.AddFinishAction(delegate
             {
                 JoyUtility.TryGainRecRoomThought(pawn);
+                if (effecter != null)
+                {
+                    effecter.Cleanup();
+                    effecter = null;
+                }
             });
-            yield return toil;
-            yield return Toils_Reserve.Release(TargetIndex.B);
-            yield return Toils_Jump.Jump(chooseCell);
+            if (isNotForJoy)
+            {
+                StudyToil.activeSkill = () => skillDef;
+            }
+            yield return StudyToil;
+            yield return Toils_Reserve.Release(CellTargetInd);
+            if (isNotForJoy)
+            {
+                yield return Toils_General.Wait(2);
+            }
+            else
+            {
+                yield return Toils_Jump.Jump(chooseCell);
+            }
+        }
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Values.Look(ref isNotForJoy, "isNotForJoy", false);
         }
 
         public override object[] TaleParameters()
@@ -61,7 +117,7 @@ namespace SimpleTrainingExpanded
             return new object[2]
             {
                 pawn,
-                base.TargetA.Thing.def
+                building.def
             };
         }
     }
